4ec5477fd410faa75d40a194fae7748c695deaa5
Fábio <fbn4sc@gmail.com>
2018-01-23 21:31:00 -0200

    Reads a file asynchronously and prints the line count

--- program.js
+++ program.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 const path = process.argv[2];
-const content = fs.readFileSync(path).toString();
-const lineCount = content.split("\n").length - 1;
 
-console.log(lineCount);
+fs.readFile(path, "utf8", (error, data) => {
+  if (error) throw error;
+
+  const lineCount = data.split("\n").length - 1;
+
+  console.log(lineCount);
+});
