c362f35737fbf3ed587747fc8cb0e7cff7812a99
Fábio Nascimento <fbn4sc@gmail.com>
2018-01-24 22:30:08 -0200

    Handles stream of data

--- program.js
+++ program.js
@@ -2,7 +2,14 @@ const http = require("http");
 const url = process.argv[2];
 
 http.get(url, response => {
+  let content = "";
+
   response.setEncoding("utf8");
 
-  response.on("data", data => console.log(data));
+  response.on("data", data => (content += data));
+
+  response.on("end", () => {
+    console.log(content.length);
+    console.log(content);
+  });
 });
