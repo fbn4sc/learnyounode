aabd5b0acd88e9efcceb20195fcb70c799e3992c
Fábio Nascimento <fbn4sc@gmail.com>
2018-01-27 21:30:00 -0200

    Processes the body of a POST request

--- program.js
+++ program.js
@@ -2,10 +2,16 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  fs
-    .createReadStream(process.argv[3])
-    .on("data", data => res.write(data))
-    .on("end", () => res.end());
+  let content = "";
+  if (req.method === "POST") {
+    req.on("data", data => {
+      content += data.toString();
+    });
+
+    req.on("end", () => {
+      res.end(content.toUpperCase());
+    });
+  }
 });
 
 server.listen(process.argv[2]);
